a305ce771edbe6a6e54a6695836126a5af34f0f9
Conor Smith <Csmith708@hotmail.com>
2021-06-30 19:43:45 -0400

    First Commit

---
+++ DoReMi/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  DoReMi
+//
+//  Created by Conor Smith on 6/30/21.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
